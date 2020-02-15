a70bc2b222c10fe29a714a4be9693c4e3d5f4985
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-15 00:06:42 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -17,6 +17,6 @@ def _age_appropriate_birthday(birthday_kids)
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
   else 
-    puts ""
-
+    puts "You are too old for this."
+  end
 end
